9800e1fa95475b09e3ffccf8b98ee551ac859cec
ManassesM <manamiui2001@gmail.com>
2021-02-12 14:18:55 -0300

    Service layer, component registration

--- src/main/java/com/manadev/restful/entities/User.java
+++ src/main/java/com/manadev/restful/entities/User.java
@@ -13,7 +13,7 @@ public class User implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
@@ -21,7 +21,7 @@ public class User implements Serializable {
 	
 	public User() {}
 
-	public User(Integer id, String name, String email, String phone, String password) {
+	public User(Long id, String name, String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -37,11 +37,11 @@ public class User implements Serializable {
 		this.name = name;
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
