e2053842463fda26a7da13033db0670fdf41cd3c
ManassesM <manamiui2001@gmail.com>
2021-02-15 11:30:33 -0300

    Payment one-to-one association

--- src/main/java/com/manadev/restful/config/TestConfig.java
+++ src/main/java/com/manadev/restful/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.manadev.restful.entities.Category;
 import com.manadev.restful.entities.Order;
 import com.manadev.restful.entities.OrderItem;
+import com.manadev.restful.entities.Payment;
 import com.manadev.restful.entities.Product;
 import com.manadev.restful.entities.User;
 import com.manadev.restful.entities.enums.OrderStatus;
@@ -88,6 +89,11 @@ public class TestConfig  implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2021-02-12T16:41:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 }
