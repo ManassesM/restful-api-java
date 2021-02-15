eac6c569ec2303575a86ea2815d23e104bb4d8e4
ManassesM <manamiui2001@gmail.com>
2021-02-15 10:29:15 -0300

    Error fixed

--- src/main/java/com/manadev/restful/config/TestConfig.java
+++ src/main/java/com/manadev/restful/config/TestConfig.java
@@ -49,21 +49,19 @@ public class TestConfig  implements CommandLineRunner{
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
-		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
-		
 		p1.getCategories().add(cat2);
-		p2.getCategories().add(cat1);
-		p2.getCategories().add(cat3);
+		p2.getCategories().addAll(Arrays.asList(cat1, cat3));
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
 
-		/*
 		cat1.getProducts().addAll(Arrays.asList(p2));
 		cat2.getProducts().addAll(Arrays.asList(p1, p5));
 		cat3.getProducts().addAll(Arrays.asList(p1, p3, p4));
-		*/
+		
+
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
 		// User and Repository
 		
